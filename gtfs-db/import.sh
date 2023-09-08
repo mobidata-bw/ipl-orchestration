3c9590b3f23cbfb5c04aa6395cbc2e92c7042bd8
Jannis R <mail@jannisr.de>
2023-09-08 13:17:49 +0200

    add gtfs_importer service [f]

--- gtfs-db/import.sh
+++ gtfs-db/import.sh
@@ -18,10 +18,10 @@ gtfs_tmp_dir="${GTFS_TMP_DIR:-/tmp/gtfs}"
 mkdir -p "$gtfs_tmp_dir"
 dest="${DEST_PATH:?missing env \$DEST_PATH var}"
 
-zip_path="$gtfs_tmp_dir/bwgesamt.gtfs.zip"
-extracted_path="$gtfs_tmp_dir/bwgesamt.gtfs"
+zip_path="$gtfs_tmp_dir/gtfs.zip"
+extracted_path="$gtfs_tmp_dir/gtfs"
 
-print_bold 'Downloading & extracting the latest "bwgesamt" GTFS feed.'
+print_bold "Downloading & extracting the GTFS feed from $GTFS_DOWNLOAD_URL."
 set -x
 
 # Using wget's -c & -N is not an option here, see https://gist.github.com/derhuerst/745cf09fe5f3ea2569948dd215bbfe1a
