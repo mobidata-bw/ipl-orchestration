f30d84e29e9ec7875cc7600674a16133f93fbaa0
Jannis R <mail@jannisr.de>
2024-01-04 17:28:06 +0100

    chore: goaccess: ignore requests from md-ipl-(p-)mon

--- bin/goaccess/import.sh
+++ bin/goaccess/import.sh
@@ -15,7 +15,9 @@ echo "importing Traefik access logs from $traefik_access_log_file into GoAccess"
 
 # Note: With 0 matches, grep exits non-0 (which would break our while loop), so we run `true` in this case.
 # Traefik logs requests on all of its entrypoints, including the internal "admin" one, which in turn receives health check requests. We only want requests to the entrypoint for public consumption "web".
+# We ignore requests from 10.70.{171,172}.20 (md-ipl-mon/md-ipl-p-mon) completely, given that they are either for monitoring or load tasting purposes.
 # Note: Passing the `-` argument to goaccess is important, otherwise it won't import data from stdin in non-TTY environments. https://github.com/allinurl/goaccess/issues/945
 cat "$traefik_access_log_file" \
   | (grep -E '"entryPointName":\s*"web"' || true) \
+  | (grep -v -E '"ClientHost":\s*"10.70.(171|172).20"' || true) \
   | goaccess --config-file=/etc/goaccess.conf --no-global-config -o /srv/goaccess/report/index.html --restore --persist --tz="$tz" -
