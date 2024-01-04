100d201f21166afde366ea633325bc0244048270
Jannis R <mail@jannisr.de>
2024-01-04 17:27:37 +0100

    fix: goaccess: handle spaces when filtering for entryPointName

--- bin/goaccess/import.sh
+++ bin/goaccess/import.sh
@@ -16,5 +16,6 @@ echo "importing Traefik access logs from $traefik_access_log_file into GoAccess"
 # Note: With 0 matches, grep exits non-0 (which would break our while loop), so we run `true` in this case.
 # Traefik logs requests on all of its entrypoints, including the internal "admin" one, which in turn receives health check requests. We only want requests to the entrypoint for public consumption "web".
 # Note: Passing the `-` argument to goaccess is important, otherwise it won't import data from stdin in non-TTY environments. https://github.com/allinurl/goaccess/issues/945
-cat "$traefik_access_log_file" | (grep '"entryPointName":"web"' || true) \
+cat "$traefik_access_log_file" \
+  | (grep -E '"entryPointName":\s*"web"' || true) \
   | goaccess --config-file=/etc/goaccess.conf --no-global-config -o /srv/goaccess/report/index.html --restore --persist --tz="$tz" -
