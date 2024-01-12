2eb3653fed6850165fc0c5532d163f7976664958
Jannis R <mail@jannisr.de>
2024-01-12 17:24:00 +0100

    fix: pgbouncer: let reload-pgbouncer-database.sh abort on DB errors

--- etc/reload-pgbouncer-databases.sh
+++ etc/reload-pgbouncer-databases.sh
@@ -19,7 +19,7 @@ set -x
 # > PgBouncer notices when a configuration file reload changes the connection parameters of a database definition. An existing server connection to the old destination will be closed when the server connection is next released (according to the pooling mode), and new server connections will immediately use the updated connection parameters.
 # > RESUME [db]
 # > Resume work from previous KILL, PAUSE, or SUSPEND command.
-psql -b -p 6432 pgbouncer <<EOF
+psql -b -v 'ON_ERROR_STOP=1' -p 6432 pgbouncer <<EOF
 SUSPEND;
 RELOAD;
 RESUME;
