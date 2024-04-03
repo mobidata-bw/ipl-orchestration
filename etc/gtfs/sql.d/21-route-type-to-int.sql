394f0cf37100576310a4511c2c4fb0761fdfa989
Jannis R <mail@jannisr.de>
2024-04-03 11:11:23 +0200

    fix: geoserver/gtfs-api: only cast route_type explicitly

--- etc/gtfs/sql.d/21-route-type-to-int.sql
+++ etc/gtfs/sql.d/21-route-type-to-int.sql
@@ -1,2 +1,4 @@
--- Tell PostgreSQL that it can implicitly convert a route_type_val (e.g. `3`) to an integer just like it would convert a string `'3'`.
-CREATE CAST (api.route_type_val AS integer) WITH INOUT AS IMPLICIT;
+-- Tell PostgreSQL that it can explicitly convert a route_type_val (e.g. `3`) to an integer just like it would convert a string `'3'`.
+-- see also https://www.postgresql.org/docs/current/sql-createcast.html
+-- see also https://stackoverflow.com/a/56746056/1072129
+CREATE CAST (api.route_type_val AS integer) WITH INOUT AS ASSIGNMENT;
