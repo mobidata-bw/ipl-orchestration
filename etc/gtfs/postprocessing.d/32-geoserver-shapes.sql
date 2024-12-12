b860c65a2f09cf3c7e096b0ede313a210e7c87da
Richard Forster <160242313+richardforsterNVBW@users.noreply.github.com>
2024-12-12 09:29:42 +0100

    added agency attributes to materialized view

--- etc/gtfs/postprocessing.d/32-geoserver-shapes.sql
+++ etc/gtfs/postprocessing.d/32-geoserver-shapes.sql
@@ -5,6 +5,9 @@ CREATE MATERIALIZED VIEW geoserver.shapes_with_routes AS
 		route_type,
 		array_to_string(array_agg(DISTINCT route_id), ', ') AS route_ids,
 		array_to_string(array_agg(DISTINCT route_name), ', ') AS route_names
+		agency_id,
+		agency_name,
+		agency_url
 	FROM (
 		SELECT DISTINCT ON (shape_id, route_id)
 			shapes.shape_id,
@@ -12,12 +15,16 @@ CREATE MATERIALIZED VIEW geoserver.shapes_with_routes AS
 			routes.route_id,
 			route_type, -- todo: normalize into basic route type?
 			coalesce(route_short_name, route_long_name) AS route_name
+			agency.agency_id,
+			agency.agency_name,
+			agency.agency_url
 		FROM api.shapes_aggregated shapes
 		JOIN api.trips ON shapes.shape_id = trips.shape_id
 		JOIN api.routes ON trips.route_id = routes.route_id
+		JOIN api.agency ON routes.agency_id = agency.agency_id
 		WHERE route_short_name NOT LIKE '%SEV%'
 	) t
-	GROUP BY shape_id, route_type;
+	GROUP BY shape_id, route_type, agency_id, agency_name, agency_url;
 
 -- allow filtering via Geoserver's CQL
 -- todo: solve properly in gtfs-via-postgres, for all enums
