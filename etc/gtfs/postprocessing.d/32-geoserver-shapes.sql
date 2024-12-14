43574669e3d998fe6eb7e383be35f7d546ebe093
Richard Forster <160242313+richardforsterNVBW@users.noreply.github.com>
2024-12-14 23:49:35 +0100

    Update 32-geoserver-shapes.sql

--- etc/gtfs/postprocessing.d/32-geoserver-shapes.sql
+++ etc/gtfs/postprocessing.d/32-geoserver-shapes.sql
@@ -5,28 +5,28 @@ CREATE MATERIALIZED VIEW geoserver.shapes_with_routes AS
 		route_type,
 		array_to_string(array_agg(DISTINCT route_id), ', ') AS route_ids,
 		array_to_string(array_agg(DISTINCT route_name), ', ') AS route_names,
-		agency.agency_id,
-		agency.agency_name,
-		agency.agency_url
+		agency_id,
+		agency_name,
+		agency_url
 	FROM (
 		SELECT DISTINCT ON (shape_id, route_id)
 			shapes.shape_id,
 			shape,
 			routes.route_id,
-			route_type, -- todo: normalize into basic route type?
+			route_type,
 			coalesce(route_short_name, route_long_name) AS route_name,
-			routes.agency_id
+			agency.agency_id,
+			agency.agency_name,
+			agency.agency_url
 		FROM api.shapes_aggregated shapes
 		JOIN api.trips ON shapes.shape_id = trips.shape_id
 		JOIN api.routes ON trips.route_id = routes.route_id
 		JOIN api.agency ON routes.agency_id = agency.agency_id
 		WHERE route_short_name NOT LIKE '%SEV%'
-	)
-	JOIN api.agency ON t.agency_id = agency.agency_id
-	GROUP BY shape_id, route_type, agency.agency_id, agency.agency_name, agency.agency_url;
+	)	
+	GROUP BY shape_id, route_type, agency_id, agency_name, agency_url;
 
 -- allow filtering via Geoserver's CQL
--- todo: solve properly in gtfs-via-postgres, for all enums
 DROP CAST IF EXISTS (api.route_type_val AS text);
 CREATE CAST (api.route_type_val AS text) WITH INOUT AS ASSIGNMENT;
 
