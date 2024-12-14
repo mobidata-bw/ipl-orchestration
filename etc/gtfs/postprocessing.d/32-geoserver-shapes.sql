9d9a1bf45b3ea4e3d159afa8e5d452b0ada36460
Richard Forster <160242313+richardforsterNVBW@users.noreply.github.com>
2024-12-14 21:10:10 +0100

    Update 32-geoserver-shapes.sql

--- etc/gtfs/postprocessing.d/32-geoserver-shapes.sql
+++ etc/gtfs/postprocessing.d/32-geoserver-shapes.sql
@@ -5,9 +5,9 @@ CREATE MATERIALIZED VIEW geoserver.shapes_with_routes AS
 		route_type,
 		array_to_string(array_agg(DISTINCT route_id), ', ') AS route_ids,
 		array_to_string(array_agg(DISTINCT route_name), ', ') AS route_names,
-		array_to_string(array_agg(DISTINCT agency_id), ', ') AS agency_ids,
-		array_to_string(array_agg(DISTINCT agency_name), ', ') AS agency_names,
-		array_to_string(array_agg(DISTINCT agency_url), ', ') AS agency_urls
+		agency.agency_id,
+		agency.agency_name,
+		agency.agency_url
 	FROM (
 		SELECT DISTINCT ON (shape_id, route_id)
 			shapes.shape_id,
@@ -15,16 +15,15 @@ CREATE MATERIALIZED VIEW geoserver.shapes_with_routes AS
 			routes.route_id,
 			route_type, -- todo: normalize into basic route type?
 			coalesce(route_short_name, route_long_name) AS route_name,
-			routes.agency_id,
-			agency.agency_name,
-			agency.agency_url
+			routes.agency_id
 		FROM api.shapes_aggregated shapes
 		JOIN api.trips ON shapes.shape_id = trips.shape_id
 		JOIN api.routes ON trips.route_id = routes.route_id
-		JOIN api.agency ON routes.agency_id = agency.agency_id	
+		JOIN api.agency ON routes.agency_id = agency.agency_id
 		WHERE route_short_name NOT LIKE '%SEV%'
-	) t
-	GROUP BY shape_id, route_type;
+	)
+	JOIN api.agency ON t.agency_id = agency.agency_id
+	GROUP BY shape_id, route_type, agency.agency_id, agency.agency_name, agency.agency_url;
 
 -- allow filtering via Geoserver's CQL
 -- todo: solve properly in gtfs-via-postgres, for all enums
