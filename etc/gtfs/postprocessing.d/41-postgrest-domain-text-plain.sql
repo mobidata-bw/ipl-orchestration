b5610585fb3ed27df0918171a501f9fd96edf070
Jannis R <mail@jannisr.de>
2024-12-18 17:16:51 +0100

    fix: gtfs-api: grant access to gtfs_data_imported_at metric to PostgREST

--- etc/gtfs/postprocessing.d/41-postgrest-domain-text-plain.sql
+++ etc/gtfs/postprocessing.d/41-postgrest-domain-text-plain.sql
@@ -1,2 +1,4 @@
 -- see https://postgrest.org/en/stable/references/api/media_type_handlers.html
 CREATE DOMAIN api."text/plain" AS text;
+
+GRANT USAGE ON DOMAIN api."text/plain" TO postgrest;
--- etc/gtfs/postprocessing.d/42-postgrest-imported-at-metric.sql
+++ etc/gtfs/postprocessing.d/42-postgrest-imported-at-metric.sql
@@ -3,3 +3,5 @@ RETURNS api."text/plain"
 AS $$
 	SELECT E'# HELP gtfs_data_imported_at when the GTFS data has been imported\n# TYPE gtfs_data_imported_at gauge\ngtfs_data_imported_at=' || floor(EXTRACT(EPOCH FROM api.gtfs_data_imported_at())) || E'\n';
 $$ LANGUAGE SQL IMMUTABLE;
+
+GRANT EXECUTE ON FUNCTION api.gtfs_data_imported_at_metric TO postgrest;
